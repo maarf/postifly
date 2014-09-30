4747959ac8605477b945e44779a4ff9a5de6b244
Martins Spilners <martins@ctrld.ch>
2014-09-30 08:56:03 +0300

    Set up desktop and note models

--- app/models/desktop.rb
+++ app/models/desktop.rb
@@ -1,2 +1,11 @@
 class Desktop < ActiveRecord::Base
+  
+  has_many :notes
+  
+  validates :name, presence: true
+  
+  before_create do
+    self.slug = name.parameterize
+  end
+  
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,14 @@
 class Note < ActiveRecord::Base
+  
+  TEXT_TYPE = "text"
+  IMAGE_TYPE = "image"
+  
+  belongs_to :desktop
+  
+  validates :note_type, presence: true
+  validates :note_type, inclusion: { in: [TEXT_TYPE, IMAGE_TYPE], message: "must be text or image" }
+  validates :content,   presence: true
+  validates :frame,     presence: true
+  validates :frame,     format: { with: /\A\d+,\d+,\d+,\d+\z/ }
+  
 end
