c5ca8fcb64b5543be68309989d6d593022ce9968
Martins Spilners <martins@ctrld.ch>
2014-10-06 22:50:16 +0300

    Add jQuery UI for draggable

--- app/assets/javascripts/stuff/content.js
+++ app/assets/javascripts/stuff/content.js
@@ -3,11 +3,9 @@ var Content = {
     var notesList = $('.notes-list');
     notesList.html('');
     
-    console.log(Postifly.notes)
     var notes = Postifly.notes.filter(function(note) {
       return note.get('desktop_id') == Postifly.currentDesktopId
     });
-    console.log(notes)
     
     notes.forEach(function(note) {
       var noteView = new NoteView({ model: note })
