4aa0c12e1fc06bea8ac7dc4d5c4d3ebf55300ca7
Martins Spilners <martins@ctrld.ch>
2014-10-07 15:26:57 +0300

    Remove validation for content presence

--- app/models/note.rb
+++ app/models/note.rb
@@ -7,7 +7,6 @@ class Note < ActiveRecord::Base
   
   validates :note_type, presence: true
   validates :note_type, inclusion: { in: [TEXT_TYPE, IMAGE_TYPE], message: "must be text or image" }
-  validates :content,   presence: true
   validates :frame,     presence: true
   validates :frame,     format: { with: /\A-?\d+,-?\d+,\d+,\d+\z/ }
   
